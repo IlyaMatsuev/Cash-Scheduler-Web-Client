062facd5e788ff7000881b3a2c1650f11b4f6f34
Ilya Matsuev <ilya.matsuev@kpn.com>
2021-04-19 01:36:28 +0300

    Delete dev credentials from login page

--- src/components/Login/PageContent/RegisterForm/RegisterForm.jsx
+++ src/components/Login/PageContent/RegisterForm/RegisterForm.jsx
@@ -6,7 +6,6 @@ import errorDefs from '../../../../utils/ErrorDefinitions';
 import {isValidNumber, onUIErrors, toFloat, useLogin} from '../../../../utils/GlobalUtils';
 import SecretField from '../../../../utils/SecretField';
 import userMutations from '../../../../graphql/mutations/users';
-import {dev} from '../../../../config';
 
 
 const RegisterForm = ({goToLogin}) => {
@@ -14,12 +13,12 @@ const RegisterForm = ({goToLogin}) => {
     const initErrorsState = {email: '', password: '', confirmPassword: ''};
     const [errors, setErrors] = useState(initErrorsState);
     const [state, setState] = useState({
-        firstName: dev.user.firstName,
-        lastName: dev.user.lastName,
-        balance: dev.user.balance,
-        email: dev.user.email,
-        password: dev.user.password,
-        confirmPassword: dev.user.password
+        firstName: '',
+        lastName: '',
+        balance: 0,
+        email: '',
+        password: '',
+        confirmPassword: ''
     });
 
     const validate = () => {
